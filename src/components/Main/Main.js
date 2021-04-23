34306690631b1542f04fab1798a725aba4e84c1e
xloppi <hlopkovalesha@yandex.ru>
2021-04-23 15:23:00 +0300

    step 5.1

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,27 +1,40 @@
 function Main() {
-    return (
-      <main className="content">
-        <section className="profile">
-          <div className="profile__avatar-container">
-            <img className="profile__avatar" src="#" alt="аватарка" />
-            <button className="profile__avatar-button"></button>
-          </div>
-          <div className="profile__info">
-            <h1 className="profile__title">Имя</h1>
-            <button type="button" className="profile__edit-button"></button>
-            <p className="profile__subtitle">О себе</p>
-          </div>
-          <button type="button" className="profile__add-button">
-          </button>
-        </section>
 
-        <section className="elements content__elements">
-          <ul className="elements__cards">
+const handleEditAvatarClick = () => {
+  document.querySelector('.popup_update-avatar').classList.add('popup_display_flex')
+}
 
-          </ul>
-        </section>
-      </main>
-    );
+const handleEditProfileClick = () => {
+  document.querySelector('.popup_edit-profile').classList.add('popup_display_flex')
+}
+
+const handleAddPlaceClick = () => {
+  document.querySelector('.popup_add-place').classList.add('popup_display_flex')
+}
+
+  return (
+    <main className="content">
+      <section className="profile">
+        <div className="profile__avatar-container">
+          <img className="profile__avatar" src="#" alt="аватарка" />
+          <button className="profile__avatar-button" onClick={handleEditAvatarClick}></button>
+        </div>
+        <div className="profile__info">
+          <h1 className="profile__title">Имя</h1>
+          <button type="button" className="profile__edit-button" onClick={handleEditProfileClick}></button>
+          <p className="profile__subtitle">О себе</p>
+        </div>
+        <button type="button" className="profile__add-button" onClick={handleAddPlaceClick}>
+        </button>
+      </section>
+
+      <section className="elements content__elements">
+        <ul className="elements__cards">
+
+        </ul>
+      </section>
+    </main>
+  );
 }
 
 export default Main;
