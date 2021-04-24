6c5016bebabfcb3914b2ed9f093a000274a07470
xloppi <hlopkovalesha@yandex.ru>
2021-04-24 11:39:18 +0300

    add: personal data request

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,35 @@
+import {useState, useEffect } from 'react';
+import api from '../../utils/api';
+
 function Main(props) {
+
+  const [userName, setUserName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
+
+  useEffect(() => {
+    api.getUserInfo()
+    .then(data => {
+      setUserName(data.name);
+      setUserDescription(data.about);
+      setUserAvatar(data.avatar);
+    })
+    .catch((err) => {
+      console.log('Ошибка: ', err);
+    });
+  }, [])
 
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar-container">
-          <img className="profile__avatar" src="#" alt="аватарка" />
+          <img className="profile__avatar" src={userAvatar} alt="аватарка" />
           <button className="profile__avatar-button" onClick={props.onEditAvatar}></button>
         </div>
         <div className="profile__info">
-          <h1 className="profile__title">Имя</h1>
+          <h1 className="profile__title">{userName}</h1>
           <button type="button" className="profile__edit-button" onClick={props.onEditProfile}></button>
-          <p className="profile__subtitle">О себе</p>
+          <p className="profile__subtitle">{userDescription}</p>
         </div>
         <button type="button" className="profile__add-button" onClick={props.onAddPlace}>
         </button>
